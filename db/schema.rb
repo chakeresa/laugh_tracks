94130b089d15cbe60cb816298aace5068b4e9f5e
Alexandra Chakeres <alexchakeres@gmail.com>
2019-05-01 14:40:17 -0600

    Add migration/schema for episodes

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190430214015) do
+ActiveRecord::Schema.define(version: 20190501190036) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,13 @@ ActiveRecord::Schema.define(version: 20190430214015) do
     t.string "name"
     t.integer "age"
     t.string "city"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "episodes", force: :cascade do |t|
+    t.string "title"
+    t.integer "viewers"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
