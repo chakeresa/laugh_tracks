449bd5f7588ba6676ca6adcd360859e5ac90599d
Alexandra Chakeres <alexchakeres@gmail.com>
2019-05-01 12:50:43 -0600

    Add model test file for Director

---
+++ spec/models/director_spec.rb
@@ -0,0 +1,9 @@
+require 'rails_helper'
+
+RSpec.describe Director, type: :model do
+  describe "validations" do
+    it {should validate_presence_of :name}
+    # TO DO: below - change to birth_year and calculate the age
+    it {should validate_presence_of :age}
+    it {should validate_presence_of :city}
+  end
