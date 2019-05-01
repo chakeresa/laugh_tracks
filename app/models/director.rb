c011e4b15be80c05b9fdcda3d12e0deb59b7dce6
Alexandra Chakeres <alexchakeres@gmail.com>
2019-05-01 14:49:51 -0600

    Add has_many & belongs_to to Director & Episodes models

--- app/models/director.rb
+++ app/models/director.rb
@@ -1,4 +1,6 @@
 class Director < ApplicationRecord
+  has_many :episodes
+  
   validates_presence_of :name
   # TO DO: below - change to birth_year and calculate the age
   validates_presence_of :age
