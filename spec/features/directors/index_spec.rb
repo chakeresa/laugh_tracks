cef74299c5beb0edc793e49e5b3ae495e24575b7
Alexandra Chakeres <alexchakeres@gmail.com>
2019-05-01 20:23:24 -0600

    Add episode count to directors/index.html.erb

--- spec/features/directors/index_spec.rb
+++ spec/features/directors/index_spec.rb
@@ -4,6 +4,7 @@ RSpec.describe "directors index page", type: :feature do
   before(:each) do
     @dir_1 = Director.create(name: "Bob Director", age: 50, city: "Chicago, IL", thumbnail: "https://resizing.flixster.com/QznwomaCPPn2qLY4qGQhpkkv95s=/679x605/v1.cjs0MzE2MjtqOzE4MDQ1OzEyMDA7Njc5OzYwNQ")
     @dir_2 = Director.create(name: "Susan Blah", age: 42, city: "Los Angeles, CA", thumbnail: "https://thefilmstage.com/wp-content/uploads/2012/02/Brian-Kirk-to-Direct-Thor-2-300x218.jpg")
+    @dir_3 = Director.create(name: "Mike McDonald", age: 30, city: "Los Angeles, CA", thumbnail: "https://upload.wikimedia.org/wikipedia/commons/thumb/5/54/Alan_Taylor_2013_crop.jpg/440px-Alan_Taylor_2013_crop.jpg")
     @eps_1 = @dir_1.episodes.create(title: "The Red Wedding", viewers: 10)
     @eps_2 = @dir_1.episodes.create(title: "Battle of the Bastards", viewers: 12)
     @eps_3 = @dir_2.episodes.create(title: "Black Water Bay", viewers: 9)
@@ -52,4 +53,68 @@ RSpec.describe "directors index page", type: :feature do
       expect(page).to have_xpath("//img[@src='#{@dir_2.thumbnail}']")
     end
   end
+
+  it "user can see episode count for each director" do
+    visit "/directors"
+
+    within "#director-#{@dir_1.id}" do
+      expect(page).to have_content("Episodes (#{@dir_1.episodes.count}):")
+    end
+
+    within "#director-#{@dir_2.id}" do
+      expect(page).to have_content("Episodes (#{@dir_2.episodes.count}):")
+    end
+
+    within "#director-#{@dir_3.id}" do
+      expect(page).to have_no_content("Episodes")
+    end
+  end
 end
+
+# User Story 4
+#
+# As a visitor
+# When I visit `/comedians?age=34`
+# Then I see the list of comedians on the page only shows
+# comedians who match the age criteria.
+#
+# - All other information on the page is still expected to be present
+# - Testing should check that excluded comedians do not show up.
+
+
+# User Story 6
+#
+# As a visitor
+# When I visit `/comedians/new`
+# Then I see a form to input a new comedian into the database
+# Including fields for their name, age and city.
+# When the form is successfully submitted and saved,
+# Then I am redirected to `/comedians`
+# And I see the new comedian's data on the page.
+
+
+# User Story 7
+#
+# As a visitor
+# When I visit `/comedians`
+# Then I see an area at the top of the page called 'Statistics'
+# In that 'Statistics' area, I see the following information:
+# - the average age of all comedians on the page (if the page is filtered for specific comedians, the statistics should reflect the new group)
+# - a unique list of cities for each comedian on the page
+#
+# Averaging and uniqueness should be done in ActiveRecord NOT
+# using Ruby
+
+
+# User Story 8
+#
+# As a visitor
+# When I visit `/comedians?age=34`
+# Then I see a list of all comedians with an age of 34
+# Just like a previous User Story, BUT all other statistics
+# information in the 'Statistics' area of the page should be limited
+# to reflect only the information about the comedians listed on
+# the page.
+#
+# - Testing should ensure that calculated statistics are
+#   correct for a limited subset of data
