9623db111e0c2f5bc0bc52dfb975289d4bda5811
Alexandra Chakeres <alexchakeres@gmail.com>
2019-05-03 21:06:46 -0600

    Update models/director_spec.rb for birth_year input (instead of age)

--- spec/models/director_spec.rb
+++ spec/models/director_spec.rb
@@ -3,11 +3,10 @@ require 'rails_helper'
 RSpec.describe Director, type: :model do
   describe "validations" do
     it {should validate_presence_of :name}
-    # TO DO: below - change to birth_year and calculate the age
-    it {should validate_presence_of :age}
+    it {should validate_presence_of :birth_year}
     it {should validate_presence_of :city}
 
-    it {should validate_numericality_of(:age).only_integer}
+    it {should validate_numericality_of(:birth_year).only_integer}
   end
 
   describe "relationships" do
@@ -16,25 +15,27 @@ RSpec.describe Director, type: :model do
 
   describe "class methods" do
     before(:each) do
-      @dir_1 = Director.create(name: "Bob Director", age: 42, city: "Chicago, IL", thumbnail: "https://resizing.flixster.com/QznwomaCPPn2qLY4qGQhpkkv95s=/679x605/v1.cjs0MzE2MjtqOzE4MDQ1OzEyMDA7Njc5OzYwNQ")
-      @dir_2 = Director.create(name: "Susan Blah", age: 42, city: "Los Angeles, CA", thumbnail: "https://thefilmstage.com/wp-content/uploads/2012/02/Brian-Kirk-to-Direct-Thor-2-300x218.jpg")
-      @dir_3 = Director.create(name: "Mike McDonald", age: 50, city: "Los Angeles, CA", thumbnail: "https://upload.wikimedia.org/wikipedia/commons/thumb/5/54/Alan_Taylor_2013_crop.jpg/440px-Alan_Taylor_2013_crop.jpg")
-      @dir_4 = Director.create(name: "Jacob Em", age: 42, city: "Aaa")
+      @dir_1 = Director.create(name: "Bob Director", birth_year: 1967, city: "Chicago, IL", thumbnail: "https://resizing.flixster.com/QznwomaCPPn2qLY4qGQhpkkv95s=/679x605/v1.cjs0MzE2MjtqOzE4MDQ1OzEyMDA7Njc5OzYwNQ")
+      @dir_2 = Director.create(name: "Susan Blah", birth_year: 1967, city: "Los Angeles, CA", thumbnail: "https://thefilmstage.com/wp-content/uploads/2012/02/Brian-Kirk-to-Direct-Thor-2-300x218.jpg")
+      @dir_3 = Director.create(name: "Mike McDonald", birth_year: 1974, city: "Los Angeles, CA", thumbnail: "https://upload.wikimedia.org/wikipedia/commons/thumb/5/54/Alan_Taylor_2013_crop.jpg/440px-Alan_Taylor_2013_crop.jpg")
+      @dir_4 = Director.create(name: "Jacob Em", birth_year: 1967, city: "Aaa")
       @eps_1 = @dir_1.episodes.create(title: "The Red Wedding", viewers: 10)
       @eps_2 = @dir_1.episodes.create(title: "Battle of the Bastards", viewers: 12)
       @eps_3 = @dir_2.episodes.create(title: "Black Water Bay", viewers: 9)
     end
 
     it "filters directors by age" do
-      expect(Director.filter_by_age(42).count).to eq(3)
-      expect(Director.filter_by_age(42)).to include(@dir_4)
-      expect(Director.filter_by_age(42)).to_not include(@dir_3)
+      current_year = Time.now.year
+      age_to_find = 1967 - current_year
+      expect(Director.filter_by_age(age_to_find).count).to eq(3)
+      expect(Director.filter_by_age(age_to_find)).to include(@dir_4)
+      expect(Director.filter_by_age(age_to_find)).to_not include(@dir_3)
 
       expect(Director.filter_by_age("zebra").count).to eq(0)
     end
 
     it "calculates average director age" do
-      Director.create(name: "Don't Add Me", age: "zebra", city: "Los Angeles, CA")
+      Director.create(name: "Don't Add Me", birth_year: "zebra", city: "Los Angeles, CA")
 
       avg_age = ((@dir_1.age + @dir_2.age + @dir_3.age + @dir_4.age).to_f / 4).round(0)
 
@@ -47,10 +48,17 @@ RSpec.describe Director, type: :model do
   end
 
   describe "instance methods" do
+    it "calculates age" do
+      dir_1 = Director.create!(name: "Bob Director", birth_year: 2009, city: "Chicago, IL")
+      current_year = Time.now.year
+
+      expect(dir_1.age).to eq(dir_1.birth_year - current_year)
+    end
+
     it "gives total episode count" do
-      dir_1 = Director.create(name: "Bob Director", age: 42, city: "Chicago, IL", thumbnail: "https://resizing.flixster.com/QznwomaCPPn2qLY4qGQhpkkv95s=/679x605/v1.cjs0MzE2MjtqOzE4MDQ1OzEyMDA7Njc5OzYwNQ")
-      dir_2 = Director.create(name: "Susan Blah", age: 42, city: "Los Angeles, CA", thumbnail: "https://thefilmstage.com/wp-content/uploads/2012/02/Brian-Kirk-to-Direct-Thor-2-300x218.jpg")
-      dir_3 = Director.create(name: "Mike McDonald", age: 50, city: "Los Angeles, CA", thumbnail: "https://upload.wikimedia.org/wikipedia/commons/thumb/5/54/Alan_Taylor_2013_crop.jpg/440px-Alan_Taylor_2013_crop.jpg")
+      dir_1 = Director.create(name: "Bob Director", birth_year: 1967, city: "Chicago, IL")
+      dir_2 = Director.create(name: "Susan Blah", birth_year: 1967, city: "Los Angeles, CA")
+      dir_3 = Director.create(name: "Mike McDonald", birth_year: 1974, city: "Los Angeles, CA")
       eps_1 = dir_1.episodes.create(title: "The Red Wedding", viewers: 10)
       eps_2 = dir_1.episodes.create(title: "Battle of the Bastards", viewers: 12)
       eps_3 = dir_2.episodes.create(title: "Black Water Bay", viewers: 9)
